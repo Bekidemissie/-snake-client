ece00c7dbe4b49144c64be6c171fe091431f81cc
Bekidemissie <bekidemissie16@gmail.com>
2023-07-28 19:42:43 -0400

    adding hundleuser input function

--- play.js
+++ play.js
@@ -5,8 +5,13 @@ console.log("Connecting ...");
 
 const setupInput = function () {
     const stdin = process.stdin;
+    stdin.on("data", handleUserInput);
     stdin.setRawMode(true);
     stdin.setEncoding("utf8");
     stdin.resume();
     return stdin;
   };
+  const handleUserInput = function(key) {
+    if (key === '\u0003') {
+      process.exit();
+    }}
